17d6cf8907c6b454bdd49aee4f8c4b6300730149
Kia <kiasambrook@btinternet.com>
2024-09-25 15:44:42 +0100

    feat: support for request caching

--- tfl_status/tflStatus.py
+++ tfl_status/tflStatus.py
@@ -1,13 +1,15 @@
 import requests
 import json
 from .Lines import *
+import requests_cache
 
 def getStatus():
     response = requests.get("https://api.tfl.gov.uk/Line/piccadilly/Status")
     return response.json()
 
 def getLines():
-    response = requests.get("https://api.tfl.gov.uk/Line/Mode/tube/Route")
+    session = requests_cache.CachedSession('tube_lines', expire_after=3600)
+    response = session.get("https://api.tfl.gov.uk/Line/Mode/tube/Route")
     data = json.loads(response.text)
     reponseLines = []
 
